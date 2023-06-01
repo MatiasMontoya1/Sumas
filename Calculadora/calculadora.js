fb3c66f4fc1dc87841309bcab73ed8ec8e4882d4
MatiasMontoya1 <115805339+MatiasMontoya1@users.noreply.github.com>
2023-06-01 09:27:53 -0300

    calculadora2

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,11 @@
+  function sumar() {
+      
+    var num1 = document.getElementById('numero1').value;
+    var num2 = document.getElementById('numero2').value;
+    
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    
+    var resultado = num1 + num2;
+    document.getElementById('resultado').textContent = 'El resultado es: ' + resultado;
+    }
