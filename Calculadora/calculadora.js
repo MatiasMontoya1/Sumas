2db5cd611ad8ec9f60c4b79d2c43aec62996d79b
MatiasMontoya1 <115805339+MatiasMontoya1@users.noreply.github.com>
2023-06-01 09:34:30 -0300

    calculadora2

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -9,3 +9,36 @@
     var resultado = num1 + num2;
     document.getElementById('resultado').textContent = 'El resultado es: ' + resultado;
     }
+    function restar() {
+      
+        var num1 = document.getElementById('numero1').value;
+        var num2 = document.getElementById('numero2').value;
+        
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
+        
+        var resultado = num1 - num2;
+        document.getElementById('resultado').textContent = 'El resultado es: ' + resultado;
+        }
+        function dividir() {
+      
+            var num1 = document.getElementById('numero1').value;
+            var num2 = document.getElementById('numero2').value;
+            
+            num1 = parseInt(num1);
+            num2 = parseInt(num2);
+            
+            var resultado = num1 / num2;
+            document.getElementById('resultado').textContent = 'El resultado es: ' + resultado;
+            }
+            function multiplicar() {
+      
+                var num1 = document.getElementById('numero1').value;
+                var num2 = document.getElementById('numero2').value;
+                
+                num1 = parseInt(num1);
+                num2 = parseInt(num2);
+                
+                var resultado = num1 * num2;
+                document.getElementById('resultado').textContent = 'El resultado es: ' + resultado;
+                }
